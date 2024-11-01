20919478932efaa71bf9f3cd2300e29c154a80ce
Miguel Lupani <mlupani2@gmail.com>
2024-11-01 11:40:50 -0300

    fin seccion 6

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,5 @@
 // 1. Import utilities from `astro:content`
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection, reference } from 'astro:content';
 
 // 2. Define a `type` and `schema` for each collection
 const blogCollection = defineCollection({
@@ -12,12 +12,30 @@ const blogCollection = defineCollection({
       message: 'Image width must be less than 1200px'
     }),
     //relaciones
-    author: z.string(),
+    //author: z.string(),
+    author: reference('author'),
     tags: z.array(z.string()),
+    isDraft: z.boolean().default(false),
+  }),
+});
+
+const authorCollection = defineCollection({
+  type: 'data',
+  schema: ({image}) =>  z.object({
+    name: z.string(),
+    avatar: image().refine(img => img.width < 1200, {
+      message: 'Image width must be less than 1200px'
+    }),
+    twitter: z.string(),
+    linkedIn: z.string(),
+    github: z.string(),
+    bio: z.string(),
+    subtitle: z.string(),
   }),
 });
 
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
   blog: blogCollection,
+  author: authorCollection
 };
